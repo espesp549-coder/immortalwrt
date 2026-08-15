2b4c1f38be0f79d39f8d7b098dac9a5525a3e07c
espesp549-coder <espesp549@gmail.com>
2026-08-15 16:55:24 +0300

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -4049,7 +4049,7 @@ define Device/qihoo_360t6m
   PAGESIZE := 2048
   KERNEL_LOADADDR := 0x80000000
   KERNEL := kernel-bin | relocate-kernel $(loadaddr-y) | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  DEVICE_PACKAGES := kmod-mt76-connac kmod-mt76-core kmod-mt7915-firmware kmod-mt7915e kmod-tun kmod-nft-tproxy wpad-basic-mbedtls luci luci-ssl dropbear
+  DEVICE_PACKAGES := kmod-mt76-connac kmod-mt76-core kmod-mt7915-firmware kmod-mt7915e kmod-tun kmod-nft-tproxy wpad-openssl luci luci-ssl dropbear
   IMAGES += factory.bin
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | check-size
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
